a6625b96be8ae5fced782b9ffd09f20600405d67
Danniel C <dnncervantes.yahoo.com>
2022-04-03 16:30:10 -0700

    first commit

---
+++ First.java
@@ -0,0 +1,7 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("My name is Danniel Cervantes");
+        System.out.println("I am 33 years old");
+        System.out.println("My hometown is Baldwin Park, CA");
+    }
+}
